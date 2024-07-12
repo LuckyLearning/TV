d9b0947ba4d84727f0e451a937af417dfebfda02
LuckyLearning <lf19950801@gmail.com>
2024-07-12 17:03:19 +0800

    Update merge_main.py

--- load/merge_main.py
+++ load/merge_main.py
@@ -1,26 +1,14 @@
 import ipaddress
-import os
 import re  # 正则
+import sys
 import urllib.request
 from datetime import datetime
 from urllib.parse import urlparse
 
+import config
+
 # 定义要访问的多个URL
-urls = [
-	'https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt',
-	'https://raw.githubusercontent.com/Guovin/TV/gd/result.txt',  # 每天自动更新1次
-	'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt',  # 每天自动更新1次
-	'https://m3u.ibert.me/txt/fmml_ipv6.txt',
-	'https://m3u.ibert.me/txt/ycl_iptv.txt',
-	'https://m3u.ibert.me/txt/y_g.txt',
-	'https://m3u.ibert.me/txt/j_home.txt',
-	'https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt',
-	'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
-	'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
-	'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',  # 1小时自动更新1次11:11 2024/05/13
-	'https://raw.githubusercontent.com/fenxp/iptv/main/live/tvlive.txt',  # 1小时自动更新1次11:11 2024/05/13
-	'https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt'
-]
+urls = config.get_urls()
 
 # 定义多个对象用于存储不同内容的行文本
 ys_lines = []  # 央视
@@ -40,6 +28,7 @@ mx_lines = []  # 明星
 ztp_lines = []  # 主题片
 zy_lines = []  # 综艺频道
 
+all_lines = []
 other_lines = []
 
 
@@ -73,9 +62,11 @@ def process_part(part_str):
 		return "CCTV-" + filtered_str
 
 	elif "卫视" in part_str:
-		pattern = r'卫视「.*」'
-		result_str = re.sub(pattern, '卫视', part_str)
-		return result_str
+		match = re.search(r'.*卫视', part_str)
+		if match:
+			return match.group(0)
+		else:
+			return part_str
 
 	return part_str
 
@@ -126,13 +117,10 @@ def process_url(url):
 					elif channel_name in zy_dictionary:
 						zy_lines.append((process_name_string(line.strip()), is_ipv6(channel_address)))
 					else:
-						other_lines.append(line.strip())
+						other_lines.append((process_name_string(line.strip()), is_ipv6(channel_address)))
 
 	except Exception as e:
 		print(f"处理URL时发生错误：{e}")
-
-
-current_directory = os.getcwd()
 
 
 def read_txt_to_array(file_name):
@@ -149,28 +137,33 @@ def read_txt_to_array(file_name):
 		return []
 
 
-dy_dictionary = read_txt_to_array('电影.txt')
-dsj_dictionary = read_txt_to_array('电视剧.txt')
-sh_dictionary = read_txt_to_array('shanghai.txt')
-gat_dictionary = read_txt_to_array('港澳台.txt')
-gj_dictionary = read_txt_to_array('国际台.txt')
-jlp_dictionary = read_txt_to_array('纪录片.txt')
-dhp_dictionary = read_txt_to_array('动画片.txt')
-xq_dictionary = read_txt_to_array('戏曲频道.txt')
-js_dictionary = read_txt_to_array('解说频道.txt')
-cw_dictionary = read_txt_to_array('春晚.txt')
-mx_dictionary = read_txt_to_array('明星.txt')
-ztp_dictionary = read_txt_to_array('主题片.txt')
-zy_dictionary = read_txt_to_array('综艺频道.txt')
-
-for url in urls:
-	print(f"处理URL: {url}")
-	process_url(url)
+def process():
+	global ys_dictionary, ws_dictionary, ty_dictionary, sh_dictionary, \
+		dy_dictionary, dsj_dictionary, gat_dictionary, gj_dictionary, \
+		jlp_dictionary, dhp_dictionary, xq_dictionary, js_dictionary, \
+		cw_dictionary, mx_dictionary, ztp_dictionary, zy_dictionary
+	dy_dictionary = read_txt_to_array('电影.txt')
+	dsj_dictionary = read_txt_to_array('电视剧.txt')
+	sh_dictionary = read_txt_to_array('shanghai.txt')
+	gat_dictionary = read_txt_to_array('港澳台.txt')
+	gj_dictionary = read_txt_to_array('国际台.txt')
+	jlp_dictionary = read_txt_to_array('纪录片.txt')
+	dhp_dictionary = read_txt_to_array('动画片.txt')
+	xq_dictionary = read_txt_to_array('戏曲频道.txt')
+	js_dictionary = read_txt_to_array('解说频道.txt')
+	cw_dictionary = read_txt_to_array('春晚.txt')
+	mx_dictionary = read_txt_to_array('明星.txt')
+	ztp_dictionary = read_txt_to_array('主题片.txt')
+	zy_dictionary = read_txt_to_array('综艺频道.txt')
+	for url in urls:
+		print(f"处理URL: {url}")
+		process_url(url)
 
 
 def extract_number(s):
+	caption = s.split(',')[0]
 	if s.startswith("CCTV"):
-		num_str = s.split(',')[0].split('-')[1]
+		num_str = caption.split('-')[1]
 		if "4K" in num_str:
 			return 19
 		elif "8K" in num_str:
@@ -184,7 +177,7 @@ def extract_number(s):
 				sort_num += 1
 			return sort_num if numbers else 999
 	else:
-		return 999
+		return abs(hash(caption))
 
 
 def sort_channels(channels):
@@ -202,10 +195,14 @@ def sort_channels_ipv6(channels):
 	return [c[0] for c in sorted_channels]
 
 
-version = datetime.now().strftime("%Y%m%d") + ",url"
-all_lines = ["更新时间,#genre#"] + [version] + ['\n'] + \
-            ["央视频道,#genre#"] + sorted(sort_channels(ys_lines), key=lambda x: extract_number(x)) + ['\n'] + \
-            ["卫视频道,#genre#"] + sort_channels_ipv6(ws_lines)
+def get_all_lines():
+	global all_lines
+	version = datetime.now().strftime("%Y%m%d") + ",url"
+	all_lines = ["更新时间,#genre#"] + [version] + ['\n'] + \
+	            ["央视频道,#genre#"] + sorted(sort_channels(ys_lines), key=lambda x: extract_number(x)) + ['\n'] + \
+	            ["卫视频道,#genre#"] + sort_channels_ipv6(ws_lines)
+
+
 # ["卫视频道,#genre#"] + sort_channels(ws_lines) + ['\n'] + \
 # ["体育频道,#genre#"] + sort_channels(ty_lines) + ['\n'] + \
 # ["上海频道,#genre#"] + sort_channels(sh_lines) + ['\n'] + \
@@ -222,46 +219,54 @@ all_lines = ["更新时间,#genre#"] + [version] + ['\n'] + \
 # ["综艺频道,#genre#"] + sort_channels(zy_lines) + ['\n'] + \
 # ["春晚,#genre#"] + sort_channels(cw_lines)
 
-output_file = "merged_output.txt"
-others_file = "others_output.txt"
-try:
-	with open(output_file, 'w', encoding='utf-8') as f:
-		for line in all_lines:
-			f.write(line + '\n')
-	print(f"合并后的文本已保存到文件: {output_file}")
+def save_to_file(output_file, others_file):
+	try:
+		# 写入合并的文本到 output_file
+		with open(output_file, 'w', encoding='utf-8') as f:
+			for line in all_lines:
+				f.write(line + '\n')
+		print(f"合并后的文本已保存到文件: {output_file}")
 
-	with open(others_file, 'w', encoding='utf-8') as f:
-		for line in other_lines:
-			f.write(line + '\n')
-		for line in ty_lines:
-			f.write(line + '\n')
-		for line in sh_lines:
-			f.write(line + '\n')
-		for line in dy_lines:
-			f.write(line + '\n')
-		for line in dsj_lines:
-			f.write(line + '\n')
-		for line in mx_lines:
-			f.write(line + '\n')
-		for line in ztp_lines:
-			f.write(line + '\n')
-		for line in gat_lines:
-			f.write(line + '\n')
-		for line in gj_lines:
-			f.write(line + '\n')
-		for line in jlp_lines:
-			f.write(line + '\n')
-		for line in dhp_lines:
-			f.write(line + '\n')
-		for line in xq_lines:
-			f.write(line + '\n')
-		for line in js_lines:
-			f.write(line + '\n')
-		for line in zy_lines:
-			f.write(line + '\n')
-		for line in cw_lines:
-			f.write(line + '\n')
-	print(f"Others已保存到文件: {others_file}")
+		# 写入其他文件内容到 others_file
+		files_to_write = [
+			(other_lines, 'other_lines'),
+			(ty_lines, 'ty_lines'),
+			(sh_lines, 'sh_lines'),
+			(dy_lines, 'dy_lines'),
+			(dsj_lines, 'dsj_lines'),
+			(mx_lines, 'mx_lines'),
+			(ztp_lines, 'ztp_lines'),
+			(gat_lines, 'gat_lines'),
+			(gj_lines, 'gj_lines'),
+			(jlp_lines, 'jlp_lines'),
+			(dhp_lines, 'dhp_lines'),
+			(xq_lines, 'xq_lines'),
+			(js_lines, 'js_lines'),
+			(zy_lines, 'zy_lines'),
+			(cw_lines, 'cw_lines')
+		]
 
-except Exception as e:
-	print(f"保存文件时发生错误：{e}")
+		with open(others_file, 'w', encoding='utf-8') as f:
+			for lines, name in files_to_write:
+				for line, ipv6 in lines:
+					f.write(line + '\n')
+				print(f"{name} 已保存到文件: {others_file}")
+
+	except Exception as e:
+		print(f"保存文件时发生错误：{e}")
+
+
+if __name__ == '__main__':
+	arguments = sys.argv
+	if len(arguments) == 1:
+		output_file = "merged_output.txt"
+		other_file = "others_output.txt"
+	elif len(arguments) == 3:
+		output_file = arguments[1]
+		other_file = arguments[2]
+	else:
+		print("Usage: python script.py [output_file] [other_file]")
+		sys.exit(1)
+	process()
+	get_all_lines()
+	save_to_file(output_file, other_file)
